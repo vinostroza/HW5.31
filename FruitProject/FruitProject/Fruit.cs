2ff92b994b54721caa18934db2c16dbe54c9da14
vinostroza <50379840+vinostroza@users.noreply.github.com>
2019-05-31 14:48:20 -0500

    Add files via upload

---
+++ FruitProject/FruitProject/Fruit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FruitProject
+{
+    public class Fruit
+    {
+        public string _size { set; get; }
+
+        public string _color { set; get; }
+
+
+        public Fruit(string size, string color)
+        {
+            _size = size;
+            _color = color;
+
+            Console.WriteLine("Delicious ready-to-eat fruit");
+        }
+
+        public void grow(string enlarge)
+        {
+            Console.WriteLine("Avocados are {0} at first, but they get {1}", _size, enlarge);
+        }
+
+    }
+
+    public class Avocado: Fruit
+    {
+         public bool _hasSeed { set; get; }
+         public  Avocado(string size, string color, bool hasSeed) : base(size, color)
+        { 
+                _size = size;
+                _color = color;
+                _hasSeed = hasSeed;
+        }
+
+
+        public void ripe(string colorChange)
+        {
+            Console.WriteLine("Avocados are {0}, but they {1} when ripe", _color, colorChange);
+        }
+
+    }
+
+}
+    
---
+++ FruitProject/FruitProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FruitProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Fruit fruta = new Fruit("green","medium");
+            fruta.grow("bigger");
+
+            Avocado palta = new Avocado("Medium","Green", true);
+            palta.ripe("turn black");     
+
+           
+        }
+    }
+}
